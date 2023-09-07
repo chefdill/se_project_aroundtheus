8f96cf81ccceffa102afaec51909cfa76f630d7e
chefdill <dillon.sareerat@gmail.com>
2023-09-07 12:49:22 -0500

    completed for review

--- scripts/index.js
+++ scripts/index.js
@@ -55,15 +55,6 @@ function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__label-title");
-  const likeButton = cardElement.querySelector(".card__like-button");
-
-  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
-  cardDeleteButton.addEventListener("click", () => {
-    cardElement.remove("card__delete-button");
-  });
-  likeButton.addEventListener("click", () => {
-    likeButton.classList.toggle("card__like-button_active");
-  });
   //set the path to the image to the link field of the object
   cardImageEl.src = cardData.link;
   //set the image alt text to the name field of the object
