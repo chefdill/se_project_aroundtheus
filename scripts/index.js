611ebd138b6c3b6aec2c50d8f78b6c2ab86ce442
chefdill <dillon.sareerat@gmail.com>
2023-09-12 20:37:09 -0500

    fixed changes

--- scripts/index.js
+++ scripts/index.js
@@ -48,7 +48,7 @@ const cardTemplate =
 
 /*Functions*/
 function closePopop() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -76,7 +76,7 @@ function handleProfileEditSubmit(e) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal_opened");
 });
 
 profileEditCloseButton.addEventListener("click", closePopop);
