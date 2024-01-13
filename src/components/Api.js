ecc154ec2c9304c165498660fb2c4376a9d1d3eb
chef_dill <dillon.sareerat@gmail.com>
2024-01-13 17:44:07 -0600

    loadInfo add

--- src/components/Api.js
+++ src/components/Api.js
@@ -19,13 +19,29 @@ class Api {
   });
 }
 }
-console.log(Api);
 
-//   {"user":
-//     {"name":"Jacques Cousteau",
-//   "about":"Sailor, researcher",
-//   "avatar":"https://practicum-content.s3.us-west-1.amazonaws.com/frontend-developer/common/avatar.jpg",
-//   "_id":"02f66c87141a521c2f243ec4"
-//     },
-//   "token":"836b8479-9e90-454e-94f6-1e85f7ca3e2f"
-//   }
+loadInfo() {
+
+    return fetch("https://around-api.en.tripleten-services.com/v1/users/me", {
+        method: "GET",
+        avatar: 
+        "https://practicum-content.s3.us-west-1.amazonaws.com/frontend-developer/moved_avatar.jpg",
+        _id: "e20537ed11237f86bbb20ccb",
+        cohort: "group-42",
+        headers: {
+            authorization: "9074552f-5489-49c7-bab8-7f6a130e519f",
+        },
+      })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+    }
+
+editProfile(){
+
+    return fetch("https://around-api.en.tripleten-services.com/v1/users/me")
+
+    }
+
+
