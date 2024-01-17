afbd02144cba00cb552813c55b56c006459bc3ba
chef_dill <dillon.sareerat@gmail.com>
2024-01-16 20:48:21 -0600

    testing

--- src/components/Api.js
+++ src/components/Api.js
@@ -20,34 +20,34 @@ class Api {
 }
 }
 
-loadInfo() {
+// loadInfo() {
 
-    return fetch("https://around-api.en.tripleten-services.com/v1/users/me", {
-        method: "GET",
-        avatar: 
-        "https://practicum-content.s3.us-west-1.amazonaws.com/frontend-developer/moved_avatar.jpg",
-        _id: "e20537ed11237f86bbb20ccb",
-        cohort: "group-42",
-        headers: {
-            authorization: "9074552f-5489-49c7-bab8-7f6a130e519f",
-        },
-      })
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-      });
-    }
+//     return fetch("https://around-api.en.tripleten-services.com/v1/users/me", {
+//         method: "GET",
+//         avatar: 
+//         "https://practicum-content.s3.us-west-1.amazonaws.com/frontend-developer/moved_avatar.jpg",
+//         _id: "e20537ed11237f86bbb20ccb",
+//         cohort: "group-42",
+//         headers: {
+//             authorization: "9074552f-5489-49c7-bab8-7f6a130e519f",
+//         },
+//       })
+//       .then((res) => res.json())
+//       .then((result) => {
+//         console.log(result);
+//       });
+//     }
 
-editProfile(){
+// editProfile(){
 
-    return fetch("https://around-api.en.tripleten-services.com/v1/users/me", {
-        method: "PATCH",
-        headers: {
-            authorization: "9074552f-5489-49c7-bab8-7f6a130e519f", 
-            "Content-type": "application/json",
-        }
-    })
+//     return fetch("https://around-api.en.tripleten-services.com/v1/users/me", {
+//         method: "PATCH",
+//         headers: {
+//             authorization: "9074552f-5489-49c7-bab8-7f6a130e519f", 
+//             "Content-type": "application/json",
+//         }
+//     })
 
-    }
+//     }
 
 
