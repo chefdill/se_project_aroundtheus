2d23e78d376fca3eb8ae03443c42132f41f67af7
chef_dill <dillon.sareerat@gmail.com>
2024-01-19 18:37:51 -0600

    auth, and _id fix

--- src/components/Api.js
+++ src/components/Api.js
@@ -6,7 +6,7 @@ class Api {
     getInitialCards() {
       return fetch("https://around-api.en.tripleten-services.com/v1", {
         headers: {
-            authorization: "9074552f-5489-49c7-bab8-7f6a130e519f"
+            authorization: "f00dbe4d-3bcf-40e3-a46f-9e1ed8206bd5"
         }
       })
       .then(res => {
@@ -24,10 +24,10 @@ loadInfo() {
         method: "GET",
         avatar: 
         "https://practicum-content.s3.us-west-1.amazonaws.com/frontend-developer/moved_avatar.jpg",
-        _id: "e20537ed11237f86bbb20ccb",
+        _id: "ccb4e0fc7796278dc190e1e9",
         cohort: "group-42",
         headers: {
-            authorization: "9074552f-5489-49c7-bab8-7f6a130e519f",
+            authorization: "f00dbe4d-3bcf-40e3-a46f-9e1ed8206bd5",
         },
       })
       .then((res) => res.json())
