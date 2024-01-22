8b42e0bf57a9cc9e47d7c53ecf51920ce949e73f
chef_dill <dillon.sareerat@gmail.com>
2024-01-22 15:40:52 -0600

    updated

--- src/components/Api.js
+++ src/components/Api.js
@@ -14,54 +14,63 @@ export default class Api {
     return Promise.reject(`Error: ${res.status}`);
   }
 
+  // 1 LOADING USER INFORMATION INTO THE SERVER
+  // GET https://around-api.en.tripleten-services.com/v1/users/me 
   getUserInfo() {
-    // return fetch(`${this._baseUrl}/users/me`, {
-    //   headers: this._headers,
-    // }).then(this._checkResponse);
-    fetch (`${this._baseUrl}/users/me`)
-    .then (response => {
-      if (response.ok) {
-        console.log("NOT THE RESPONSE");
-      }
-      return response.json();
-    })
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then(this._checkResponse);
   }
 
+  // 2 LOADING CARDS FROM THE SERVER
+  // GET https://around-api.en.tripleten-services.com/v1/cards 
+  // initial response will be an empty JSON array []
   getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
       headers: this._headers,
     }).then(this._checkResponse);
   }
 
-}
-// 1 LOADING USER INFORMATION INTO THE SERVER
-// GET https://around-api.en.tripleten-services.com/v1/users/me 
-  //{  **this should be the response if working correctly** 
-  //   "name": "Jacques Cousteau",
-  //   "about": "Sailor, researcher",
-  //   "avatar": "https://practicum-content.s3.us-west-1.amazonaws.com/frontend-developer/moved_avatar.jpg",
-  //   "_id": "e20537ed11237f86bbb20ccb",
-  //   "cohort": "group-42"
-  // } 
-
-// 2 LOADING CARDS FROM THE SERVER
-// GET https://around-api.en.tripleten-services.com/v1/cards 
-    // initial response will be an empty JSON array []
-
-// 3  EDITING THE PROFILE 
-//  PATCH https://around-api.en.tripleten-services.com/v1/users/me 
+  // 3  EDITING THE PROFILE 
+  //  PATCH https://around-api.en.tripleten-services.com/v1/users/me 
   // Add Content-type after the authorization token 
-    // body: JSON.stringify ({NAME and ABOUT}) 
-
-
-// 4 ADDING A NEW CARD
-// POST https://around-api.en.tripleten-services.com/v1/cards
+  // body: JSON.stringify ({NAME and ABOUT}) 
+  getEditProfile(name, about) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name, 
+        about: about
+      })
+      
+    }).then(this._checkResponse);
+  }
+  // 4 ADDING A NEW CARD
+  // POST https://around-api.en.tripleten-services.com/v1/cards
   // body: JSON.stringify ({NAME and LINK})
+  getNewCard(name, link) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify ({
+        name: name,
+        link: link
+      })
+    })
+
+}
 
 // 5 & 6 CREATING A NEW POPUP FOR DELETING A CARD
   // send DELETE request 
     // DELETE https://around-api.en.tripleten-services.com/v1/cards/cardId 
-    
+    deleteCard(cardId) {
+      return fetch(`${this._baseUrl}/cards/${cardId}`, {
+        method: "DELETE",
+        headers: this._headers,
+      }).then(this._checkResponse);
+    }
+  }
     // The cardId in the URL should be replaced with the _id parameter of the card to be deleted. 
     // The _id of each card is found in its respective JSON:
     // {
@@ -72,6 +81,7 @@ export default class Api {
 
     // Request to deleting card looks like this
     //DELETE https://around-api.en.tripleten-services.com/v1/cards/5d1f0611d321eb4bdcd707dd
+  
 
 
 // 7 - 9 look back at program lets get 1 - 6 done first
