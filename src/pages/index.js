908831d59f2082c19711ae274a2b62825cf90972
chef_dill <dillon.sareerat@gmail.com>
2024-01-22 22:57:42 -0600

    adding requests

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithDelete from "../components/PopupWithDelete.js";
 import UserInfo from "../components/UserInfo.js";
 import Api from "../components/Api.js";
 
@@ -66,11 +67,64 @@ const addCardPopup = new PopupWithForm(
 
 //functions
 
-function handleAddCardFormSubmit(formData) {
-  const card = createCard({ name: formData.title, link: formData.url });
-  cardSection.addItem(card);
-  addCardPopup.close();
+function createCard(cardData) {
+  const card = new Card(
+    cardData,
+    "#card-template",
+    handleImageClick,
+    handleDeleteClick,
+    handleLikeClick
+  );
+  section.addItem(card.generateCard());
 }
+
+function setButtonText(button, text) {
+  button.textContent = text;
+}
+
+function handleAddCardFormSubmit(inputValues) {
+  setButtonText(addNewCardButton, "Saving...");
+  api
+    .addNewCard(inputValues)
+    .then((res) => {
+      console.log(res);
+      // create a new card
+      createCard(res);
+      // render it
+      addNewModalCard.reset();
+      // toggle button state
+      cardFormValidator.toggleButtonState();
+      // close modal
+      addCardPopup.close();
+    })
+    .catch(console.error)
+    .finally(() => {
+      setButtonText(addNewCardButton, "Save");
+    });
+
+    const deleteModal = new PopupWithDelete(variables.deleteCardClass);
+    deleteModal.setEventListeners();
+
+function handleDeleteClick(card) {
+  // open the modal
+  deleteModal.open();
+  // set the submit action
+  deleteModal.setSubmitAction(() => {
+    api
+      .deleteCard(card._id)
+      .then(() => {
+        deleteModal.close();
+        card.handleDelete();
+      })
+      .catch(console.error);
+  });
+}
+}
+// function handleAddCardFormSubmit(formData) {
+//   const card = createCard({ name: formData.title, link: formData.url });
+//   cardSection.addItem(card);
+//   addCardPopup.close();
+// }
 
 function createCard(cardData) {
   return new Card(cardData, "#card-template", () => {
@@ -97,6 +151,34 @@ api.getInitialCards().then((cards) => {
     cardSection.addItem(cardElemet);
   });
 });
+
+let section;
+
+api
+  .getInitialCards()
+  .then((res) => {
+    section = new Section(
+      {
+        items: res,
+        renderer: (cardData) => {
+          createCard(cardData);
+        },
+      },
+      "gallery__cards"
+    );
+    section.renderItems();
+    console.log(res);
+  })
+  .catch(console.error);
+
+api
+  .loadInfo()
+  .then((res) => {
+    console.log(res);
+    userInfo.setUserInfo(res.name, res.about);
+    userInfo.setAvatar(res.avatar);
+  })
+  .catch(console.error);
 
 //Event Listeners
 
