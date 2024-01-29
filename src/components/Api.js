aac802e694c4339cfaa88c6a98ab8bb429993d07
chef_dill <dillon.sareerat@gmail.com>
2024-01-29 15:37:51 -0600

    updated api

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 export default class Api {
   constructor() {
     this._baseUrl = "https://around-api.en.tripleten-services.com/v1";
@@ -11,22 +10,6 @@ export default class Api {
   _getResponse(res) {
     if (res.ok) {
       return res.json();
-=======
-class Api {
-    constructor(options) {
-      this._header = options._headers;
-    }
-  
-    getInitialCards() {
-      return fetch("https://around-api.en.tripleten-services.com/v1", {
-        headers: {
-            authorization: "f00dbe4d-3bcf-40e3-a46f-9e1ed8206bd5"
-        }
-      })
-      .then(res => {
-        if (res.ok) {
-            return res.json();
->>>>>>> Stashed changes
     }
     return Promise.reject(`Error: ${res.status}`);
   }
@@ -39,7 +22,6 @@ class Api {
     }).then(this._checkResponse);
   }
 
-<<<<<<< Updated upstream
   // 2 LOADING CARDS FROM THE SERVER
   // GET https://around-api.en.tripleten-services.com/v1/cards 
   // initial response will be an empty JSON array []
@@ -60,17 +42,6 @@ class Api {
       body: JSON.stringify({
         name, 
         about: about
-=======
-    return fetch("https://around-api.en.tripleten-services.com/v1/users/me", {
-        method: "GET",
-        avatar: 
-        "https://practicum-content.s3.us-west-1.amazonaws.com/frontend-developer/moved_avatar.jpg",
-        _id: "ccb4e0fc7796278dc190e1e9",
-        cohort: "group-42",
-        headers: {
-            authorization: "f00dbe4d-3bcf-40e3-a46f-9e1ed8206bd5",
-        },
->>>>>>> Stashed changes
       })
       
     }).then(this._checkResponse);
@@ -107,17 +78,28 @@ class Api {
         body: JSON.stringify({ avatarUrl: avatarUrl }),
       }).then(this._checkResponse);
     }
-  }
-    // The cardId in the URL should be replaced with the _id parameter of the card to be deleted. 
-    // The _id of each card is found in its respective JSON:
-    // {
-    //   "isLiked": false,
-    //   "_id": "5d1f0611d321eb4bdcd707dd", // — here it is
-    //   // ...other card data
-    // }
 
-    // Request to deleting card looks like this
-    //DELETE https://around-api.en.tripleten-services.com/v1/cards/5d1f0611d321eb4bdcd707dd
+    updateLike(cardId, isLiked) {
+      return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        method: isLiked ? "DELETE" : "PUT",
+        headers: this._headers,
+      }).then(this._checkResponse);
+    }
+  
+    updateAvatar(avatar) {
+      return fetch(`${this._baseUrl}/users/me/avatar`, {
+        method: "PATCH",
+        headers: this._headers,
+        body: JSON.stringify({
+          avatar: avatar,
+        }),
+      }).then(this._checkResponse);
+    }
+
+
+
+  }
+
   
 
 
