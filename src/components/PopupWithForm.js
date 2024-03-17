64f8bf20cd675c3adb3b2c186ea1d769fb8a7b66
chef_dill <dillon.sareerat@gmail.com>
2024-03-17 16:47:02 -0500

    changed .modal__input for #add-card-modal

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,7 +5,7 @@ export default class PopupWithForm extends Popup {
         super({ popupSelector });
         this._handleFormSubmit = handleFormSubmit;
         this._popupForm = this._popupElement.querySelector(".modal__form")
-        this._inputEls = [...this._popupForm.querySelectorAll(".modal__input")];
+        this._inputEls = [...this._popupForm.querySelectorAll("#add-card-modal")];
     }
 
     close() {
