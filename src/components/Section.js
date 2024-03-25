a808a4cb3b803d42721f994660084b265d534fa4
chef_dill <dillon.sareerat@gmail.com>
2024-03-25 15:44:32 -0500

    deploying takes forever

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,8 +2,8 @@ class Section {
   constructor({ items, renderer }, containerSelector) {
     this._items = items;
     this._renderer = renderer;
-    this._container = document.querySelector(containerSelector);
     console.log(containerSelector);
+    this._container = document.querySelector(containerSelector);
   }
 
   renderItems() {
